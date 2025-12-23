5ccc64e0f386ef200f3bf64677a137b82669ce7f
Dexter Lyman <dexterlyman99@gmail.com>
2025-12-23 15:22:15 +0100

    linux

--- bash_scripting/test.sh
+++ bash_scripting/test.sh
@@ -2,13 +2,13 @@
 echo "initialize"
 mkdir images documents programs
 mv *.png images
-mv *.jpg images
-mv *.doc documents
-mv *.docx documents
-mv *.pdf documents
-mv *.ppz documents
-mv *.exe programs
-mv *.msi programs
+# mv *.jpg images
+# mv *.doc documents
+# mv *.docx documents
+# mv *.pdf documents
+# mv *.ppz documents
+# mv *.exe programs
+# mv *.msi programs
 if [ $? -eq 0 ]; then 
   echo "Success!"
 else
