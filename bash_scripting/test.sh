a5f039adddac3ad901ac0d8070d0f8d6d6410f88
Dexter Lyman <dexterlyman99@gmail.com>
2025-12-23 14:43:40 +0100

    bash

--- bash_scripting/test.sh
+++ bash_scripting/test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-
-
-mkdir "pictures"
-
+echo "initialize"
+mkdir images documents programs
+mv *.png images
+mv *.jpg images
+mv *.doc documents
+mv *.docx documents
+mv *.pdf documents
+mv *.ppz documents
+mv *.exe programs
+mv *.msi programs
+if [ $? -eq 0 ]; then 
+  echo "Success!"
+else
+  echo "ERROR!"
+fi
